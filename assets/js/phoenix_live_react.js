5ffda11c26985171d8a86d1c535318583427bf5b
Josh Ison <ison.84@buckeyemail.osu.edu>
2022-09-20 16:30:46 -0400

    Trying to convert to react 18

--- assets/js/phoenix_live_react.js
+++ assets/js/phoenix_live_react.js
@@ -1,31 +1,41 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-const render = function(el, target, componentClass, additionalProps = {}, previousProps = {}) {
-  let props = el.dataset.liveReactProps ? JSON.parse(el.dataset.liveReactProps) : {};
+const render = function (
+  el,
+  target,
+  componentClass,
+  additionalProps = {},
+  previousProps = {}
+) {
+  let props = el.dataset.liveReactProps
+    ? JSON.parse(el.dataset.liveReactProps)
+    : {};
   if (el.dataset.liveReactMerge) {
-    props = {...previousProps, ...props, ...additionalProps}
+    props = { ...previousProps, ...props, ...additionalProps };
   } else {
-    props = {...props, ...additionalProps}
+    props = { ...props, ...additionalProps };
   }
   const reactElement = React.createElement(componentClass, props);
-  ReactDOM.render(reactElement, target);
+  const root = createRoot(target);
+  root.render(reactElement);
   return props;
-}
+};
 
-const initLiveReactElement = function(el, additionalProps) {
+const initLiveReactElement = function (el, additionalProps) {
   const target = el.nextElementSibling;
   const componentClass = eval(el.dataset.liveReactClass);
   render(el, target, componentClass, additionalProps);
-  return {target: target, componentClass: componentClass};
-}
+  return { target: target, componentClass: componentClass };
+};
 
-const initLiveReact = function() {
-  const elements = document.querySelectorAll('[data-live-react-class]')
-  Array.prototype.forEach.call(elements, el => {
-    initLiveReactElement(el)
+const initLiveReact = function () {
+  const elements = document.querySelectorAll("[data-live-react-class]");
+  Array.prototype.forEach.call(elements, (el) => {
+    initLiveReactElement(el);
   });
-}
+};
 
 const LiveReact = {
   mounted() {
@@ -34,8 +44,12 @@ const LiveReact = {
     const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
     const handleEvent = this.handleEvent && this.handleEvent.bind(this);
     const { target, componentClass } = initLiveReactElement(el, { pushEvent });
-    const props = render(el, target, componentClass, { pushEvent, pushEventTo, handleEvent });
-    if (el.dataset.liveReactMerge) this.props = props
+    const props = render(el, target, componentClass, {
+      pushEvent,
+      pushEventTo,
+      handleEvent,
+    });
+    if (el.dataset.liveReactMerge) this.props = props;
     Object.assign(this, { target, componentClass });
   },
 
@@ -45,14 +59,20 @@ const LiveReact = {
     const pushEventTo = this.pushEventTo && this.pushEventTo.bind(this);
     const handleEvent = this.handleEvent;
     const previousProps = this.props;
-    const props = render(el, target, componentClass, { pushEvent, pushEventTo }, previousProps);
-    if (el.dataset.liveReactMerge) this.props = props
+    const props = render(
+      el,
+      target,
+      componentClass,
+      { pushEvent, pushEventTo },
+      previousProps
+    );
+    if (el.dataset.liveReactMerge) this.props = props;
   },
 
   destroyed() {
     const { target } = this;
-    ReactDOM.unmountComponentAtNode(target);
-  }
-}
+    target.unmount();
+  },
+};
 
 export { LiveReact as default, initLiveReact, initLiveReactElement };
